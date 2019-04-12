000c291febddcf61bedc2295608da6090df93b43
Roberto Martins <roberto.martins.info@gmail.com>
2019-04-11 21:55:47 -0300

    Set websocket uri.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const API_ROOT = 'https://secret-lake-76496.herokuapp.com';
-export const API_WS_ROOT = 'ws://secret-lake-76496.herokuapp.com/cable';
+export const API_WS_ROOT = 'wss://secret-lake-76496.herokuapp.com/cable';
 export const HEADERS = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
