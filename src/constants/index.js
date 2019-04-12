4fb38e3f44c49dc13ce8f3c625c66ef6c4b6d54d
Roberto Martins <roberto.martins.info@gmail.com>
2019-04-11 21:40:02 -0300

    Create components of conversation and cable.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const API_ROOT = 'http://localhost:3000';
-export const API_WS_ROOT = 'ws://localhost:3000/cable';
+export const API_ROOT = 'https://secret-lake-76496.herokuapp.com';
+export const API_WS_ROOT = 'ws://secret-lake-76496.herokuapp.com/cable';
 export const HEADERS = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
