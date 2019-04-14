16eae46fc4433abd9e560d886126da543a7f657a
Roberto Martins <roberto.martins.info@gmail.com>
2019-04-14 07:04:00 -0300

    Change constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,11 @@
+export const DEV_API_ROOT = 'https://chat-api-d3.herokuapp.com';
+export const DEV_API_WS_ROOT = 'wss://chat-api-d3.herokuapp.com/cable';
+
 export const API_ROOT = 'https://chat-api-d3.herokuapp.com';
 export const API_WS_ROOT = 'wss://chat-api-d3.herokuapp.com/cable';
 
-// export const DEV_API_ROOT = 'https://chat-api-d3.herokuapp.com';
-// export const DEV_API_WS_ROOT = 'wss://chat-api-d3.herokuapp.com/cable';
-
-export const DEV_API_ROOT = 'http://localhost:3000';
-export const DEV_API_WS_ROOT = 'ws://localhost:3000/cable';
+// export const DEV_API_ROOT = 'http://localhost:3000';
+// export const DEV_API_WS_ROOT = 'ws://localhost:3000/cable';
 
 export const HEADERS = {
     'Content-Type': 'application/json',
